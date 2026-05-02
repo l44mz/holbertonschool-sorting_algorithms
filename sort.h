81f6cd047dc2c8c2d74f28023a92afd633ae2e00
janaalhazmi <janaalhazmi78@gmail.com>
2026-05-02 15:20:03 +0000

    Add : task 0 : Bubble sort

---
+++ sort.h
@@ -0,0 +1,19 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* print functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+/* prototypes*/
+void bubble_sort(int *array, size_t size);
+
+#endif
